c668c4e1e50e6f32b45d7089e0897770ba990e47
lidongldld <277388472@qq.com>
2018-04-17 05:34:24 +0800

    removeDuplicates finished

--- removeDuplicates/removeDuplicates.c
+++ removeDuplicates/removeDuplicates.c
@@ -1,22 +1,20 @@
 #include<stdio.h>
 int removeDuplicates(int* nums, int numsSize) {
-	int temp = *nums;
-	for(int i = 1;i<numsSize;i++){
-		if(temp!=nums[i]){
-			temp = nums[i];
-		}else{
-			for(int j= i;j+1<numsSize;j++){
-				nums[j] = nums[j+1];
-			}
-			numsSize--;
-		}
+	int number = 0;
+	for(int i = 0;i<numsSize;i++){
+		if(nums[number]!=nums[i]){
+			number++;
+			nums[number] = nums[i];
+		}		
 	}
-	return numsSize;
+	number++;
+	return number;
 }
 
 int main(){
-	int test[] = {0,0,1,1,1,2,2,3,3,4};
-	int size = sizeof(test);
+	int test[10] = {0,0,1,1,1,2,2,3,3,4};
+	int size = sizeof(test)/4;
+	//printf("size:%d",size);
 	int result = removeDuplicates(test,size);
 	printf("result is :%d\n",result);
 	for(int i=0;i<result;i++){
